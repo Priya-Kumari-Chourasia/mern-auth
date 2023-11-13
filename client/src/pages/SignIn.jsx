46075047882ec16687a698322d16f6b4bf4ac815
Priya Kumari <priya417789@gmail.com>
2023-11-13 19:59:02 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 
 import {Link, useNavigate } from 'react-router-dom'
+import { signInStart, signInSuccess,signInFailure } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  //const [error, setError] = useState(false);
+  //const [loading, setLoading] = useState(false);
+
+  const {loading,error} = useSelector((state) => state.user);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({...formData,[e.target.id]:e.target.value})
   }
@@ -14,24 +20,25 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
+
       const res = await fetch('/api/auth/signin',{
         method: 'POST',
         headers: { 'Content-Type':'application/json',},
         body: JSON.stringify(formData),
       })
       const data = await res.json();
-      //console.log(data);
-      setLoading(false);
+
+      
       if (data.success === false) {
-        setError(true);
+      
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate('/');
     }catch(error){
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error));
     }
    
   }
@@ -67,7 +74,7 @@ export default function SignIn() {
           <span className='text-blue-500'>Sign up</span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+      <p className='text-red-700 mt-5'> {error ? error.message || 'Something went wrong!' : ''}</p>
     
     </div>
   )
